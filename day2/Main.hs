66f361a0d9f44bc9837038272a85a8621a02469e
David Tennander <david.tennander@gmail.com>
2024-12-01 20:51:21 +0100

    Prepare for day2

---
+++ day2/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Aoc (runner)
+
+main :: IO ()
+main = runner id solve1 solve2
+
+solve1 :: String -> String
+solve1 = undefined
+
+solve2 :: String -> String
+solve2 = undefined
