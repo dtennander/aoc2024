50905f2b997144518a287a5f858fefb737baaef1
David Tennander <david.tennander@gmail.com>
2024-12-03 16:09:41 +0100

    Add day 2

--- day2/Main.hs
+++ day2/Main.hs
@@ -3,10 +3,37 @@ module Main where
 import Aoc (runner)
 
 main :: IO ()
-main = runner id solve1 solve2
+main = runner parseReports p1 p2
+ where
+  p1 = solve False
+  p2 = solve True
 
-solve1 :: String -> String
-solve1 = undefined
+type Report = [Level]
+type Level = Int
 
-solve2 :: String -> String
-solve2 = undefined
+parseReports :: String -> [Report]
+parseReports = fmap parseLine . lines
+ where
+  parseLine = fmap read . words
+
+solve :: Bool -> [Report] -> Int
+solve p2 = sum . fmap (fromEnum . isSafe p2)
+
+isSafe :: Bool -> Report -> Bool
+isSafe p2 (x : y : rest) = failed $ foldl' checkEach initState (y : rest)
+ where
+  initState = FoldState{failed = False, lastLevel = x, haveDroppedOne = False}
+  checkEach s n
+    | 1 <= diff && diff <= 3 = s{lastLevel = n}
+    | p2 && not (haveDroppedOne s) = s{haveDroppedOne = True}
+    | otherwise = s{failed = True}
+   where
+    diff = direction * ((lastLevel s) - n)
+  direction = max (-1) . min 1 $ (x - y)
+isSafe _ _ = False
+
+data FoldState = FoldState
+  { failed :: Bool
+  , lastLevel :: Level
+  , haveDroppedOne :: Bool
+  }
--- src/Aoc.hs
+++ src/Aoc.hs
@@ -6,4 +6,4 @@ runner :: (Show b, Show c) => (String -> a) -> (a -> b) -> (a -> c) -> IO ()
 runner parser p1 p2 = do
   args <- getArgs
   let program = if "-p2" `elem` args then show . p2 else show . p1
-  interact (program . parser)
+  interact (program . parser) >> putStrLn ""
