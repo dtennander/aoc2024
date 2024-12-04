6ca5c874ab76d226dda9c3657cb43bd63ccb245b
David Tennander <david.tennander@gmail.com>
2024-12-05 00:24:23 +0100

    Day4 🧹

--- day4/Main.hs
+++ day4/Main.hs
@@ -1,55 +1,56 @@
 module Main where
 
 import Aoc (runner)
-import Control.Monad (guard)
+import Control.Monad (guard, join)
 
 main :: IO ()
 main = runner lines p1 p2
 
 type Input = [[Char]]
-type Point = (Int, Int)
-type Direction = (Int, Int)
 
 p1 :: Input -> Int
-p1 input = length . filter (== "XMAS") $ paths
+p1 input = length . filter (== "XMAS") $ pathsAsStrings input paths
  where
   width = length input
   height = length (input !! 0)
-  paths = do
-    x <- [0 .. width]
-    y <- [0 .. height]
-    d1 <- [-1 .. 1]
-    d2 <- [-1 .. 1]
-    guard (d1 /= 0 || d2 /= 0)
-    let p = path (x, y) (d1, d2) 4 input
-    guard (length p == 4)
-    return p
+  paths = join [linePaths (x, y) | x <- [0 .. width], y <- [0 .. height]]
 
 p2 :: Input -> Int
-p2 input = length . filter (\ps -> 2 == (length . filter (== "MAS") $ ps)) $ paths
+p2 input = length . filter ((== 2) . (length . filter (== "MAS"))) $ fmap (pathsAsStrings input) paths
  where
   width = length input
   height = length (input !! 0)
-  xPaths (x, y) = do
-    d1 <- [-1, 1]
-    d2 <- [-1, 1]
-    let p = path (x - d1, y - d2) (d1, d2) 3 input
-    guard (length p == 3)
-    return p
-  paths = do
-    x <- [0 .. width]
-    y <- [0 .. height]
-    return $ xPaths (x, y)
+  paths = [xPaths (x, y) | x <- [0 .. width], y <- [0 .. height]]
 
-path :: Point -> Direction -> Int -> [[a]] -> [a]
-path (x, y) (d1, d2) l input = do
-  let width = length input
-  let height = length (input !! 0)
+type Point = (Int, Int)
+type Path = [Point]
+
+xPaths :: Point -> [Path]
+xPaths (x, y) = do
+  d1 <- [-1, 1]
+  d2 <- [-1, 1]
+  return $ path (x - d1, y - d2) (d1, d2) 3
+
+linePaths :: Point -> [Path]
+linePaths (x, y) = do
+  d1 <- [-1 .. 1]
+  d2 <- [-1 .. 1]
+  guard (d1 /= 0 || d2 /= 0)
+  return $ path (x, y) (d1, d2) 4
+
+path :: Point -> (Int, Int) -> Int -> Path
+path (x, y) (d1, d2) l = do
   i <- [0 .. (l - 1)]
   let x' = x + i * d1
   let y' = y + i * d2
-  guard (0 <= x')
-  guard (0 <= y')
-  guard (x' < width)
-  guard (y' < height)
-  return $ (input !! x') !! y'
+  return $ (x', y')
+
+pathsAsStrings :: [String] -> [Path] -> [String]
+pathsAsStrings input = fmap withValue . filter (all inside)
+ where
+  width = length input
+  height = length (input !! 0)
+  inside (x, y) = 0 <= x && x < width && 0 <= y && y < height
+  withValue ps = do
+    (x, y) <- ps
+    return $ (input !! x) !! y
